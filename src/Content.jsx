fdf6ab9bfd4b5dbee18743170a10eca6a889fe88
DerrickDevelops <derrickdevelops@gmail.com>
2024-03-29 21:16:11 -0400

    Fixed Javascript Issue
    Removed JS file, converted file into MainContent file.

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,31 +1,55 @@
-import React from 'react'
-import './Layout.css'
+import React, { useState } from 'react';
+import './Layout.css';
 
 const MainContent = () => {
+    // State to store the input value
+    const [inputValue, setInputValue] = useState('');
+
+    // Function to handle the conversion
+    const handleConvert = () => {
+        // Convert the input value here
+        const meterValue = (inputValue * 3.218).toFixed(2);
+        const feetValue = (inputValue * 0.304).toFixed(2);
+        const literValue = (inputValue * 0.264).toFixed(2);
+        const gallonValue = (inputValue * 3.785).toFixed(2);
+        const kiloValue = (inputValue * 2.2).toFixed(2);
+        const poundsValue = (inputValue * 0.453).toFixed(2);
+
+        // Update the content of elements with the "info" class
+        document.getElementById('conversionleng').innerText = `${inputValue} Meters = ${meterValue} Feet | ${inputValue} Feet = ${feetValue} Meters`;
+        document.getElementById('conversionvol').innerText = `${inputValue} Liters = ${literValue} Gallons | ${inputValue} Gallons = ${gallonValue} Liters`;
+        document.getElementById('conversionmass').innerText = `${inputValue} Kilos = ${kiloValue} Pounds | ${inputValue} Pounds = ${poundsValue} Kilograms`;
+    };
+
+    // Function to handle input change
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    };
+
     return (
         <main className="content-container">
             <div className="inputbox-container">
-            <h3>Metric & Imperial</h3>
-            <p>Unit conversion</p>
-				<input type="number" id="inputvalue"/><br />
-				<button className="convertbtn" id="btn" onClick="convertValue()">convert</button>
+                <h3>Metric & Imperial</h3>
+                <p>Unit conversion</p> 
+                <input type="number" id="inputvalue" value={inputValue} onChange={handleInputChange} /><br />
+                <button className="convertbtn" id="btn" onClick={handleConvert}>Convert</button>
             </div>
             <div className="results-container">
-            <div className="length results" id="leng">
-					<h5 className="title">Length (Meters/Feet)</h5>
-					<p className="info" id="conversionleng"></p>
-				</div>
-				<div className="volume results" id="vol">
-					<h5 className="title">Volume (Liters/Gallons)</h5>
-					<p className="info" id="conversionvol"></p>
-				</div>
-				<div className="mass results" id="mass">
-					<h5 className="title">Mass (Kilograms/Pounds)</h5>
-					<p className="info" id="conversionmass"></p>
-				</div>
+                <div className="length results" id="leng">
+                    <h5 className="title">Length (Meters/Feet)</h5>
+                    <p className="info" id="conversionleng"></p>
+                </div>
+                <div className="volume results" id="vol">
+                    <h5 className="title">Volume (Liters/Gallons)</h5>
+                    <p className="info" id="conversionvol"></p>
+                </div>
+                <div className="mass results" id="mass">
+                    <h5 className="title">Mass (Kilograms/Pounds)</h5>
+                    <p className="info" id="conversionmass"></p>
+                </div>
             </div>
         </main>
-    )
+    );
 }
 
-export default MainContent
+export default MainContent;
--- src/conversion.js
+++ src/conversion.js
@@ -1,36 +1,37 @@
-/*
-1 meter = 3.281 feet
-1 liter = 0.264 gallon
-1 kilogram = 2.204 pound
-document.body.appendChild(convertedEl)
-*/
+// import React from 'react';
+// /*
+// 1 meter = 3.281 feet
+// 1 liter = 0.264 gallon
+// 1 kilogram = 2.204 pound
+// document.body.appendChild(convertedEl)
+// */
 
-let inputValue = document.getElementById('inputvalue');
-let btnEl = document.getElementById('btn');
-let infoEl = document.querySelector('info');
-let lengEl = document.getElementById('conversionleng');
-let volEl = document.getElementById('conversionvol')
-let massEl = document.getElementById('conversionmass')
+// // let inputValue = document.getElementById('inputvalue');
+// let btnEl = document.getElementById('btn');
+// let infoEl = document.querySelector('info');
+// let lengEl = document.getElementById('conversionleng');
+// let volEl = document.getElementById('conversionvol')
+// let massEl = document.getElementById('conversionmass')
 
-function convertValue() {
-	let value = parseFloat(inputValue.value);
+// function convertValue() {
+// 	const inputValue = parseFloat(document.getElementById('inputvalue').value)
 
 
-    let meterValue = (value * 3.218).toFixed(2);
-    let feetValue = (value * 0.304).toFixed(2)
+//     let meterValue = (value * 3.218).toFixed(2);
+//     let feetValue = (value * 0.304).toFixed(2)
    
-   lengEl.innerText = value + " " + "Meters = " + meterValue + " Feet" + " | " + value + " " + "Feet = " + feetValue + " Meters";
+//    lengEl.innerText = value + " " + "Meters = " + meterValue + " Feet" + " | " + value + " " + "Feet = " + feetValue + " Meters";
 
-   literValue = (value * 0.264).toFixed(2);
-   gallonValue = (value * 3.785).toFixed(2)
+//    literValue = (value * 0.264).toFixed(2);
+//    gallonValue = (value * 3.785).toFixed(2)
    
-   volEl.innerText = value + " " + "Liters = " + literValue + " Gallons" + " | " + value + " " + "Gallons = " + gallonValue + " Liters";
+//    volEl.innerText = value + " " + "Liters = " + literValue + " Gallons" + " | " + value + " " + "Gallons = " + gallonValue + " Liters";
 
-   kiloValue = (value * 2.2).toFixed(2);
-   poundsValue = (value * 0.453).toFixed(2)
+//    kiloValue = (value * 2.2).toFixed(2);
+//    poundsValue = (value * 0.453).toFixed(2)
    
-   massEl.innerText = value + " " + "Kilos = " + kiloValue + " Pounds" + " | " + value + " " + "Pounds = " + poundsValue + " Kilograms";
+//    massEl.innerText = value + " " + "Kilos = " + kiloValue + " Pounds" + " | " + value + " " + "Pounds = " + poundsValue + " Kilograms";
 
+// }
 
-	// console.log(typeof value)
-}
+// export default convertValue;
