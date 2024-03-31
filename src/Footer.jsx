763eb0a15b4e2051eb9dee0f61cca08902a8b3b4
DerrickDevelops <derrickdevelops@gmail.com>
2024-03-30 23:13:50 -0400

    Contact Links
    Added Contact Links to Footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './Layout.css'
 
+
 const Footer = () => {
     return (
         <footer className="footer-container">
             <h6>Made By Derrick Develops</h6>
+            <div class="contact-links">
+            <a href='https://github.com/DerrickDevelops'><i class="fa-brands fa-github"></i></a>
+            <a href='https://www.linkedin.com/in/derrick-murray-753a66233'><i class="fa-brands fa-linkedin"></i></a>
+            <a href='https://derrickdevelops.com/'><i class="fa-solid fa-globe"></i></a>
+            </div>
         </footer>
     )
 }
